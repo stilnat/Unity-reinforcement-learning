05a8e5294854008d90c2efba1add44b0a6f7029e
stilnat <nathan.raynal.castang@gmail.com>
2022-09-29 10:45:59 +0200

    New cartpole3D training environment

---
+++ Assets/Cartpole3D/CartPoleController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CartPoleController : MonoBehaviour
+{
+    ArticulationBody _Rigidbody;
+    public float _Thrust = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _Rigidbody = GetComponent<ArticulationBody>();
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey("up"))
+        {
+            //Apply a force to this Rigidbody in direction of this GameObjects up axis
+            _Rigidbody.AddForce(transform.forward *_Thrust);
+        }
+
+        if (Input.GetKey("down"))
+        {
+            //Apply a force to this Rigidbody in direction of this GameObjects up axis
+            _Rigidbody.AddForce(-transform.forward * _Thrust);
+        }
+
+        if (Input.GetKey("left"))
+        {
+            //Apply a force to this Rigidbody in direction of this GameObjects up axis
+            _Rigidbody.AddForce(-transform.right * _Thrust);
+        }
+
+        if (Input.GetKey("right"))
+        {
+            //Apply a force to this Rigidbody in direction of this GameObjects up axis
+            _Rigidbody.AddForce(transform.right * _Thrust);
+        }
+
+
+
+
+    }
+}
