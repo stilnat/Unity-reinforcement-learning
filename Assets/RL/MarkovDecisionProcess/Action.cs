79d05aa88fae5e6ab0708b146a2ca7b46f3fea71
stilnat <nathan.raynal.castang@gmail.com>
2022-09-29 13:53:05 +0200

    removes useless using statement in action.cs

--- Assets/RL/MarkovDecisionProcess/Action.cs
+++ Assets/RL/MarkovDecisionProcess/Action.cs
@@ -1,7 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 /// <summary>
 /// An action in a Markov decision process, represented by a matrix.
 /// </summary>
