e18def612ff69321d19abf7ea5bdfd0cc13f9446
stilnat <nathan.raynal.castang@gmail.com>
2022-10-02 00:04:45 +0200

    cartpole training try to read Json to get back data

--- Assets/Cartpole3D/CartPoleTraining.cs
+++ Assets/Cartpole3D/CartPoleTraining.cs
@@ -1,20 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class CartPoleTraining : MonoBehaviour
 {
 
     private Agent _agent;
-    private Dictionary<State, Dictionary<EnvironmentAction, StateActionValue>> _actionStateValue;
+    private QValueCollection _actionStateValue;
     private EnvironmentPolicy _policyToLearn;
     private EnvironmentPolicy _policyToFollow;
 
-    // Start is called before the first frame update
-    void Start()
+   // Start is called before the first frame update
+   void Start()
     {
         _agent = gameObject.GetComponent<Agent>();
-        _actionStateValue = new Dictionary<State, Dictionary<EnvironmentAction, StateActionValue>>();
+        if (File.Exists(@".\test.json"))
+        {
+            string json = File.ReadAllText(@".\test.json");
+            _actionStateValue = QValueCollection.CreateFromJSON(json);
+        }
+        else _actionStateValue = new QValueCollection();
+
         _policyToLearn = new EnvironmentPolicy();
         _policyToFollow = new EnvironmentPolicy();
     }
@@ -32,11 +37,22 @@ public class CartPoleTraining : MonoBehaviour
 
         if (_agent.State.IsTerminal == false)
         {
-            var res = QLearning.QLearningPolicyWithUnityOneStep(_actionStateValue, _agent.State, _agent, _policyToFollow, _policyToLearn, 0.99f, 0.2f, 0.1f);
+            var res = QLearning.QLearningPolicyWithUnityOneStep(_actionStateValue, _agent.State, _agent, _policyToFollow, _policyToLearn, 0.99f, 0.7f, 0.1f);
             
             _policyToLearn = res.Item2;
             _actionStateValue = res.Item3;
         }
 
     }
+
+    private void OnApplicationQuit()
+    {
+        Debug.Log("on application quit");
+
+        string jsonString = JsonUtility.ToJson(_actionStateValue);
+        File.WriteAllText(@".\test.json", jsonString);
+    }
+
+
+
 }
