8b0259be093a25036d3d3cda1e676d7af6a5263e
stilnat <nathan.raynal.castang@gmail.com>
2022-10-03 17:59:00 +0200

    update environmentAction

--- Assets/RL/Environment/EnvironmentAction.cs
+++ Assets/RL/Environment/EnvironmentAction.cs
@@ -2,26 +2,21 @@ using System;
 public class EnvironmentAction
 {
 
-    private System.Action<Action> _actionToDo;
-    private System.Action _actionToDoNoParameters;
+    public System.Action _actionToDoNoParameters;
 
-    public EnvironmentAction(System.Action<Action> actionToDo)
-    {
-        _actionToDo = actionToDo;
-    }
 
     public EnvironmentAction(System.Action actionToDo)
     {
         _actionToDoNoParameters = actionToDo;
     }
 
-    public void Execute(Action actionParameters)
-    {
-        _actionToDo(actionParameters);
-    }
-
     public void Execute()
     {
         _actionToDoNoParameters();
+    }
+
+    public override string ToString()
+    {
+        return _actionToDoNoParameters.Method.Name;
     }
 }
