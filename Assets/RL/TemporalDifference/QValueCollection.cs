377262441006377be074dbe7408200cfdb03f99c
stilnat <nathan.raynal.castang@gmail.com>
2022-10-03 17:56:52 +0200

    add tostring method to qvaluecollection, corrects a bug in infoToCollection

--- Assets/RL/TemporalDifference/QValueCollection.cs
+++ Assets/RL/TemporalDifference/QValueCollection.cs
@@ -1,19 +1,30 @@
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 [System.Serializable]
 public class QValueCollection : 
     Dictionary<State, Dictionary<EnvironmentAction, StateActionValue>>, ISerializationCallbackReceiver
 {
+    
+    [System.Serializable]
+    private class QValueCollectionInfo
+    {
+        [SerializeField]
+        public List<State> KeyStates = new List<State>();
+
+        [SerializeField]
+        public List<StateActionValue> values = new List<StateActionValue>();
+
+        [SerializeField]
+        public List<string> KeyEnvironmentActionsName = new List<string>();
+
+        [SerializeField]
+        public List<int> ActionNumberPerState = new List<int>();
+    }
 
     [SerializeField]
-    private List<State> KeyStates = new List<State>();
-
-    [SerializeField]
-    private List<StateActionValue> values = new List<StateActionValue>();
-
-    [SerializeField]
-    private List<EnvironmentAction> KeyEnvironmentActions = new List<EnvironmentAction>();
+    private QValueCollectionInfo info = new QValueCollectionInfo();
 
     public QValueCollection()
     {
@@ -23,16 +34,18 @@ public class QValueCollection :
 
     public void OnBeforeSerialize()
     {
-        KeyStates.Clear();
-        values.Clear();
-        KeyEnvironmentActions.Clear();
+        info.KeyStates.Clear();
+        info.values.Clear();
+        info.KeyEnvironmentActionsName.Clear();
         foreach (KeyValuePair<State, Dictionary<EnvironmentAction, StateActionValue>> pair in this)
         {
-            KeyStates.Add(pair.Key);
+            info.KeyStates.Add(pair.Key);
+            info.ActionNumberPerState.Add(pair.Value.Count);
             foreach(KeyValuePair<EnvironmentAction, StateActionValue> innerPair in pair.Value)
             {
-                values.Add(innerPair.Value);
-                KeyEnvironmentActions.Add(innerPair.Key);
+                info.values.Add(innerPair.Value);
+                var name = innerPair.Key._actionToDoNoParameters.Method.Name;
+                info.KeyEnvironmentActionsName.Add(name);
             }
            
         }
@@ -40,22 +53,45 @@ public class QValueCollection :
 
     public void OnAfterDeserialize()
     {
-        this.Clear();
-        
-        for (int j = 0; j < KeyStates.Count; j++)
+
+    }
+
+    public void infoToCollection(Agent agent)
+    {
+        var type = agent.GetType();
+
+        for (int j = 0; j < info.KeyStates.Count; j++)
         {
             var dic = new Dictionary<EnvironmentAction, StateActionValue>();
-            for (int i = 0; i < KeyEnvironmentActions.Count; i++)
+            for (int i = j; i < j+info.ActionNumberPerState[j]; i++)
             {
-                dic.Add(KeyEnvironmentActions[i], values[i]);
+                string name = type.GetMethod(info.KeyEnvironmentActionsName[i]).Name;
+                System.Action action = (System.Action) Delegate.CreateDelegate(typeof(System.Action), agent, name);
+                dic.Add(new EnvironmentAction(action), info.values[i]);
             }
-            this.Add(KeyStates[j], dic);
+            this.Add(info.KeyStates[j], dic);
         }
     }
 
     public static QValueCollection CreateFromJSON(string jsonString)
     {
         return JsonUtility.FromJson<QValueCollection>(jsonString);
+    }
+
+    public override string ToString()
+    {
+        string str = "";
+        foreach (KeyValuePair<State, Dictionary<EnvironmentAction, StateActionValue>> pair in this)
+        {
+           
+            foreach (KeyValuePair<EnvironmentAction, StateActionValue> innerPair in pair.Value)
+            {
+                str += "State = " + pair.Key.ToString() + " : Action = " + innerPair.Key.ToString() + " Value = " + innerPair.Value.ToString() + " . \n";
+            }
+
+        }
+
+        return str;
     }
 
 }
